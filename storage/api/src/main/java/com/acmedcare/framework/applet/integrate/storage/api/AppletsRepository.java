72306045bd3eccf80d517bb8ed5630654eff0f67
misselvexu <iskp.me@gmail.com>
2019-09-22 22:51:25 +0800

    add applets repository supports

---
+++ storage/api/src/main/java/com/acmedcare/framework/applet/integrate/storage/api/AppletsRepository.java
@@ -0,0 +1,21 @@
+package com.acmedcare.framework.applet.integrate.storage.api;
+
+import com.acmedcare.framework.applet.integrate.common.spi.Extensible;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link AppletsRepository}
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019/9/20.
+ */
+@Extensible
+public abstract class AppletsRepository {
+
+  /** Logger Instance */
+  private static final Logger log = LoggerFactory.getLogger(AppletsRepository.class);
+
+
+
+}
